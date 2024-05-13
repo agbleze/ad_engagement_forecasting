b399ed7ff3b59acadac4d2d6f3bb3805c75d3835
linus <lagbleze@st.ug.edu.gh>
2024-05-13 23:56:20 +0200

    added explore.py

---
+++ explore.py
@@ -0,0 +1,31 @@
+#%%
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+
+#%%
+data = "data/Train.csv"
+
+
+#%%
+df = pd.read_csv(data)
+
+#%%
+df.describe()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# %%
