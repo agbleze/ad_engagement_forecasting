4b405571ef88ece7f788fdeca214f4f4fa33ef9a
linus <lagbleze@st.ug.edu.gh>
2024-05-14 23:53:09 +0200

    grouped df

--- explore.py
+++ explore.py
@@ -7,6 +7,8 @@ import matplotlib.pyplot as plt
 #%%
 data = "data/Train.csv"
 
+sub_data_path = "data/SampleSubmission.csv"
+
 
 #%%
 df = pd.read_csv(data)
@@ -14,11 +16,43 @@ df = pd.read_csv(data)
 #%%
 df.describe()
 
+#%%
+df.info()
+
+#%%
+sub_df = pd.read_csv(sub_data_path)
+
+#%%
+sub_df.info()
+
+#%%
+df.head(10)
+
+#%%
+sub_df.ID.nunique()
+
+#%%
+df.ID.nunique()
+
+#%%
+df.clicks.plot()
+
+#%%
+df.ID.unique()
+
+#%%
+df[df.ID=='ID_5da86e71bf5dee4cf5047046'].clicks.plot()
+#%%
+
+df.groupby("ID")["clicks"].plot()
 
 
 
+#%%
+"""
+ID should be used to group data
 
-
+"""
 
 
 
